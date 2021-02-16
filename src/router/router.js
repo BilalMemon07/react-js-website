a051909a1762a76666513a679e642b8da845cee8
BilalMemon07 <bilalsaylani451@gmail.com>
2021-02-16 15:01:46 +0500

    first commit

---
+++ src/router/router.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {BrowserRouter as Router,Route,} from "react-router-dom";
+import Start from '../screens/start/start';
+import First from '../screens/first/first';
+import Second from '../screens/second/second';
+import Options from '../screens/option/options';
+import Options1to1 from '../screens/option/optiions1to1';
+import Options1to1of1 from '../screens/option/options1to1of1';
+import Habit from '../screens/option/habit';
+import Navs from '../screens/navs/navs';
+
+
+class AppRouter extends React.Component{
+    render(){
+        return(
+            <Router>
+                <Route exact path="/" component={Start} />
+                <Route path="/welcome" component={First} />
+                <Route path="/night" component={Second}  />
+                <Route path="/options" component={Options} />
+                <Route path="/question1of1" component={Options1to1} />
+                <Route path="/question1of1ofmore" component={Options1to1of1} />
+                <Route path="/habits" component={Habit} />
+                <Route path="/Navs" component={Navs}/>
+            </Router>
+        )
+    }
+}
+
+export default AppRouter ;
